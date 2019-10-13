b223838d08ee2ef98cd98c6c6d2a4e61603d6acf
JorisVanEijden <joris.vaneijden@gmail.com>
2019-10-13 20:13:00 +0200

    Still struggling and experimenting.

--- Assets/Scripts/Editor/QuestEditor/Nodes/Actions/CreateFoeNode.cs
+++ Assets/Scripts/Editor/QuestEditor/Nodes/Actions/CreateFoeNode.cs
@@ -1,16 +1,17 @@
-﻿using DaggerfallWorkshop.Game.Questing;
+﻿using System;
+using DaggerfallWorkshop.Game.Questing;
 using UnityEngine;
 
 [CreateNodeMenu("Action/CreateFoe")]
 public class CreateFoeNode : ActionNode
 {
-    [Input] public string foe;
+    [Input] public FoeNode foe;
 
     // @TODO: convert minutes to days.hours:minutes and visa versa
     public uint spawnInterval;
 
     public int spawnMaxTimes;
-    [Range(0, 100)] public int spawnChance;
+    [Range(0, 100)] public int spawnChance = 100;
     public int msgMessageID;
 
     public override ActionTemplate GetAction()
@@ -19,12 +20,12 @@ public class CreateFoeNode : ActionNode
         action.RestoreSaveData(GetSaveData());
         return action;
     }
-
+    
     protected override object GetSaveData()
     {
         return new CreateFoe.SaveData_v1
         {
-            foeSymbol = new Symbol(foe),
+            foeSymbol = new Symbol(foe.name),
             lastSpawnTime = 0,
             spawnInterval = spawnInterval,
             spawnMaxTimes = spawnMaxTimes,
@@ -33,5 +34,10 @@ public class CreateFoeNode : ActionNode
             isSendAction = false,
             msgMessageID = msgMessageID
         };
+    }
+
+    protected override Type GetActionType()
+    {
+        return typeof(CreateFoe);
     }
 }
---
+++ Assets/Scripts/Editor/QuestEditor/Nodes/ISymbolize.cs
@@ -0,0 +1,4 @@
+public interface ISymbolize
+{
+    string Symbol { get ; set ; }
+}
